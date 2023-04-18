55b834b851c5f0f2eda8e7ae7d675c4de9377637
Alexander <alrlucano@gmail.com>
2023-04-17 22:55:28 -0500

    Lógica de calculadora agregada, revisar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
 import { useState } from "react";
 import Arrow from "./assets/icon-arrow.svg";
 import "./App.css";
-
+const monthsUtil = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 function App() {
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+  const [result, setResult] = useState({
+    year: "-",
+    month: "-",
+    days: "-",
+  });
+  console.log(typeof year);
+  const ageCalculator = () => {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentDay = currentDate.getDate();
+
+    let resultAge = currentYear - Number(year);
+    let resultMonth;
+    let resultDay;
+    if (currentMonth >= month) {
+      resultMonth = currentMonth - Number(month);
+    } else {
+      resultAge--;
+      resultMonth = 12 + currentMonth - Number(month);
+    }
+
+    if (currentDay >= day) {
+      resultDay = currentDay - Number(day);
+    } else {
+      resultMonth--;
+      let days = monthsUtil[currentMonth - 2];
+      resultDay = days + currentDay - Number(day);
+      if (resultMonth < 0) {
+        resultMonth = 11;
+        resultDay--;
+      }
+    }
+
+    setResult({
+      year: resultAge,
+      month: resultMonth,
+      days: resultDay,
+    });
+  };
+
   return (
     <div className="App">
       <div className="contenedor">
@@ -43,21 +87,21 @@ function App() {
           <div className="contenedor-linea">
             <div className="linea"></div>
             <div className="img-arrow">
-              <img src={Arrow} alt="arrow" />
+              <img src={Arrow} alt="arrow" onClick={() => ageCalculator()} />
             </div>
           </div>
 
           <div className="container-result">
             <div className="container-result-age">
-              <p className="number">-</p>
+              <p className="number">{result.year}</p>
               <p className="label-result">years</p>
             </div>
             <div className="container-result-age">
-              <p className="number">-</p>
+              <p className="number">{result.month}</p>
               <p className="label-result">months</p>
             </div>
             <div className="container-result-age">
-              <p className="number">-</p>
+              <p className="number">{result.days}</p>
               <p className="label-result">days</p>
             </div>
           </div>
